37cc87a417e99d180ec4b5c215ec7d7ea1ff68ec
imojha <pas074bct044@wrc.edu.np>
2023-08-05 18:07:26 +0545

    basic setup

---
+++ shared/card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+type Props = {
+  product: any;
+};
+const Card = ({ product }: Props) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg p-20 flex flex-col space-y-4">
+      <Image src={`${product.image}`} width={200} height={200} alt="Product Image" />
+      <div className="px-6 py-4 space-y-4">
+        <div className="font-bold text-xl mb-2">{product.title}</div>
+        <p>Price: {parseInt(product.price)}</p>
+        <button
+          style={{ backgroundColor: "#592C8C" }}
+          className="p-4"
+          onClick={() => console.log("hello")}
+        >
+          BUY NOW
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
